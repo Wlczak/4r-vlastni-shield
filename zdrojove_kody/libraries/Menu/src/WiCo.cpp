70772a6fb59c579a287b18243d03512e01f97357
Adam Vlček <vlcek-adam@seznam.cz>
2024-08-30 19:08:34 +0200

    added status checking

--- zdrojove_kody/libraries/Menu/src/WiCo.cpp
+++ zdrojove_kody/libraries/Menu/src/WiCo.cpp
@@ -8,6 +8,7 @@ void WiCo::startAP()
 {
     if (!isAPActive)
     {
+        WiFi.mode(WIFI_AP);
         setAPsettings();
         WiFi.softAP(ap_ssid, ap_psk, ap_ch, ap_hide_ssid, ap_max_devices);
         WiFi.softAPIP();
@@ -29,27 +30,35 @@ String WiCo::getAPIP()
 
 void WiCo::connectSTA()
 {
-    WiFi.mode(WIFI_STA);
-    setSTAsettings();
-    for (int i = 0; i < sta_ssid.size(); i++)
+    if (!isSTAActive)
     {
-        wifiMulti.addAP(sta_ssid.at(i), sta_psk.at(i));
-    }
-    for (int i = 0; i < 30; i++)
-    {
-        if (wifiMulti.run(sta_timeout) == WL_CONNECTED)
+        WiFi.mode(WIFI_STA);
+        setSTAsettings();
+        for (int i = 0; i < sta_ssid.size(); i++)
         {
-            Serial.println("");
-            Serial.print("WiFi connected: ");
-            Serial.print(WiFi.SSID());
-            Serial.print(" ");
-            Serial.println(WiFi.localIP());
-            break;
+            wifiMulti.addAP(sta_ssid.at(i), sta_psk.at(i));
         }
+        for (int i = 0; i < 30; i++)
+        {
+            if (wifiMulti.run(sta_timeout) == WL_CONNECTED)
+            {
+                Serial.println("");
+                Serial.print("WiFi connected: ");
+                Serial.print(WiFi.SSID());
+                Serial.print(" ");
+                Serial.println(WiFi.localIP());
+                break;
+            }
+        }
+        isSTAActive = true;
     }
 }
 
 void WiCo::disconnectSTA()
 {
-    WiFi.disconnect();
+    if (isSTAActive)
+    {
+        WiFi.disconnect();
+        isSTAActive = false;
+    }
 }

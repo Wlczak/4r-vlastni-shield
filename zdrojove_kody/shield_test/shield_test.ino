6000802ba3e70c61e072d00747d9007fbe26aa5c
Adam Vlček <vlcek-adam@seznam.cz>
2024-09-23 22:13:58 +0200

    shield tests

---
+++ zdrojove_kody/shield_test/shield_test.ino
@@ -0,0 +1,27 @@
+void IRAM_ATTR pain() {
+  Serial.println(digitalRead(D6));
+  Serial.println(digitalRead(D7));
+  Serial.println("digitalRead(D6)");
+}
+void IRAM_ATTR pain2() {
+  Serial.println(digitalRead(D5));
+  Serial.println("digitalRead(D5)");
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A0, INPUT);
+  pinMode(D0, OUTPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(D5, INPUT_PULLUP);
+  pinMode(D6, INPUT);
+  pinMode(D7, INPUT);
+  Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(D6), pain, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(D5), pain2, CHANGE);
+  //digitalWrite(D0, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}

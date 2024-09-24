d6dd2a9f1e53227071490ad7cfff8551c57abae3
Adam Vlček <vlceka@spseplzen.cz>
2024-09-24 11:23:33 +0200

    Update shield_test.ino

--- zdrojove_kody/shield_test/shield_test.ino
+++ zdrojove_kody/shield_test/shield_test.ino
@@ -1,24 +1,7 @@
-void IRAM_ATTR pain() {
-  Serial.println(digitalRead(D6));
-  Serial.println(digitalRead(D7));
-  Serial.println("digitalRead(D6)");
-}
-void IRAM_ATTR pain2() {
-  Serial.println(digitalRead(D5));
-  Serial.println("digitalRead(D5)");
-}
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(A0, INPUT);
   pinMode(D0, OUTPUT);
   pinMode(D3, OUTPUT);
-  pinMode(D5, INPUT_PULLUP);
-  pinMode(D6, INPUT);
-  pinMode(D7, INPUT);
-  Serial.begin(9600);
-  attachInterrupt(digitalPinToInterrupt(D6), pain, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(D5), pain2, CHANGE);
   digitalWrite(D0, HIGH);
 }
 
